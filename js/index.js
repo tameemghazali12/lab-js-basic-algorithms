aac58d9b954a7ec23c11776b7c224ef87067ab56
Girlearnlab <rambodonothing@gmail.com>
2024-08-30 10:39:02 +0530

    done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,23 @@
 // Iteration 1: Names and Input
+
+let hacke1="Tamim";
+console.log("The driver's name is "+hacke1);
+
+let hacke2="Ghazali";
+console.log("The driver's name is "+hacke2);
+
+
 //
 // Iteration 2: Conditionals
+let firstPlayerLenght=hacke1.length;
+let secondPlayerLength=hacke2.length;
 
+if(firstPlayerLenght>secondPlayerLength){
+    console.log("The driver has the longest name, it has"+firstPlayerLenght +"XX characters.");
+}else if(firstPlayerLenght<secondPlayerLength){
+    console.log("It seems that the navigator has the longest name, it has "+secondPlayerLength+" characters.")
+}else{
+    console.log("Wow, you both have equally long names, "+firstPlayerLenght +" characters!");
+}
 
 // Iteration 3: Loops
