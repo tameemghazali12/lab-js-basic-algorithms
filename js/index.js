102afd8110ced8b8a73f77637db3a96a2d085e3a
Girlearnlab <rambodonothing@gmail.com>
2024-09-01 13:05:38 +0530

    array method

--- js/index.js
+++ js/index.js
@@ -21,3 +21,59 @@ if(firstPlayerLenght>secondPlayerLength){
 }
 
 // Iteration 3: Loops
+
+// 3.1 Print all the characters of the driver's name, separated by a space and in uppercase
+
+let driverNameUpperCase=hacke1.toUpperCase();
+let spcaeDriverName=driverNameUpperCase.split("").join(" ");
+console.log(spcaeDriverName);
+
+// 3.2 Print all the characters of the navigator's name in reverse order
+let reversedName=hacke2.split("").reverse().join("");
+console.log(reversedName);
+
+// 3.3 Depending on the lexicographic order of the strings
+
+if(hacke1<hacke2){
+    console.log("The driver's name goes first.");
+}
+else if(hacke1>hacke2){
+    console.log("Yo, the navigator goes first definitely.");
+}
+else{
+    console.log("What?! You both have the same name?");
+}
+
+
+//Bonus Time!
+//Bonus 1:
+// Generate 3 paragraphs of text
+let text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euismod lorem et risus cursus, ut lacinia ligula euismod. Integer sit amet sollicitudin tortor. Nulla facilisi. Cras viverra sem sit amet felis tincidunt, ut varius libero sodales. Vivamus efficitur, nisi a suscipit efficitur, quam nisl elementum magna, et commodo tortor purus nec urna. Ut facilisis nisl et orci sagittis gravida. Fusce aliquam erat sed eros pharetra, ac cursus sapien varius. Nam et arcu et nulla interdum suscipit.`;
+
+let wordCount=text.split(/\s+/).length; //Count the number of words
+let etCount=(text.match(/et/g) || []).length; // Count occurrences of "et"
+
+console.log("Number of words: " + wordCount);
+console.log('Number of times "et" appears: ' + etCount);
+
+
+//Bonus 2:
+
+// Create a variable phraseToCheck
+let phraseToCheck = "A man, a plan, a canal, Panama!";
+
+function isPalindrome(str){
+    // Remove non-alphanumeric characters and convert to lowercase
+    let cleanedStr=str.replace(/[^a-zA-Z0-9]/g,"").toLowerCase();
+
+    // Reverse the cleaned string
+    let reversedStr = cleanedStr.split("").reverse().join("");
+
+    return cleanedStr===reversedStr;
+}
+
+if(isPalindrome(phraseToCheck)){
+    console.log("This pharase is palindrom");
+}else{
+    console.log("This pharase is not palindrom");
+}
